107eb1dec1838551410870eec2487b0a12ee4681
Ivan Wu <ivan.wu@elmosoftware.com.au>
2022-07-01 09:43:54 +1000

    create non-class-based providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,22 @@ import { AppService } from './app.service';
 import { ShoppingLibModule } from '@app/shopping-lib';
 import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
+import { PRODUCT, Product_Token } from './product/product.token';
+import { dbConnectionFactory } from './connection.provider';
+import { ClientService } from './client/client.service';
 
 @Module({
     imports: [ShoppingLibModule],
     controllers: [AppController, ProductController],
     providers: [
+        {
+            provide: PRODUCT,
+            useValue: Product_Token,
+        },
         AppService, 
         ProductService,
+        dbConnectionFactory,
+        ClientService
     ],
 })
 export class AppModule { }
